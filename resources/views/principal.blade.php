e615e7fe2c082f2d43516c6e5908873cc32bb3cc
Gustavo Jordan <gustavojordanra@gmail.com>
2020-02-03 23:22:19 -0500

    cambio organizacion del codigo

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -26,65 +26,65 @@
         
 
            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Sis Pro</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+                <a class="navbar-brand" href="#">Sis Pro</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
-      </li>
-      
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Nombre Modulo 1
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Proceso 1</a>
-          <a class="dropdown-item" href="#">Proceso 2</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Otro proceso</a>
-        </div>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Nombre Modulo 2
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Proceso 1</a>
-          <a class="dropdown-item" href="#">Proceso 2</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Otro proceso</a>
-        </div>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Administración
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Cambiar contraseña de Usuario</a>
-          <a class="dropdown-item" href="#">Asignar Rol a Usuario</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Otro proceso</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Horarios de atención</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Busqueda</button>
-    </form>
-  </div>
-</nav>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                    </li>
+                    
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Nombre Modulo 1
+                      </a>
+                      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Proceso 1</a>
+                        <a class="dropdown-item" href="#">Proceso 2</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Otro proceso</a>
+                      </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Nombre Modulo 2
+                      </a>
+                      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Proceso 1</a>
+                        <a class="dropdown-item" href="#">Proceso 2</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Otro proceso</a>
+                      </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Administración
+                      </a>
+                      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Cambiar contraseña de Usuario</a>
+                        <a class="dropdown-item" href="#">Asignar Rol a Usuario</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Otro proceso</a>
+                      </div>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Horarios de atención</a>
+                    </li>
+                  </ul>
+                  <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Busqueda</button>
+                  </form>
+                </div>
+              </nav>
 
-<p class="lead">
+              <p class="lead">
                     <div class="col-xs-12">
                     <h1 class="mt-5 ml-5">Pagina de Inicio</h1>
                 
