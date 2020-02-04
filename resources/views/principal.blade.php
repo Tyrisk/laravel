6456874dd7dc64ca83f1d83e1185cd5aa5f156c5
Gustavo Jordan <gustavojordanra@gmail.com>
2020-02-03 23:17:16 -0500

    Cambios iniales para las plantillas de los modulos
    agregado bootstrap, agregado menu de inicio, cambio de pagina de inicio

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <title>@yield('title') - Sis Pro V1.0</title>
+
+        <!-- Fonts -->
+
+
+        <!-- Styles -->
+   
+        <!-- Bootstrap core CSS -->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Custom styles for this template -->
+    <link href="sticky-footer-navbar.css" rel="stylesheet">
+     
+   </style>
+
+    
+    </head>
+   
+    <body>
+        
+
+           <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Sis Pro</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+      </li>
+      
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Nombre Modulo 1
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Proceso 1</a>
+          <a class="dropdown-item" href="#">Proceso 2</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Otro proceso</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Nombre Modulo 2
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Proceso 1</a>
+          <a class="dropdown-item" href="#">Proceso 2</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Otro proceso</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Administración
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Cambiar contraseña de Usuario</a>
+          <a class="dropdown-item" href="#">Asignar Rol a Usuario</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Otro proceso</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Horarios de atención</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Busqueda</button>
+    </form>
+  </div>
+</nav>
+
+<p class="lead">
+                    <div class="col-xs-12">
+                    <h1 class="mt-5 ml-5">Pagina de Inicio</h1>
+                
+                
+                            
+                </div>                  
+                    @yield ('contenido')
+                    
+                    
+                </p>
+                    
+             
+                 <p>
+                    <br /><br />
+                 </p>
+                
+                @section ('footer1')
+                    <div class="footer">
+                        <span class="text-muted">www.utp.ac.pa all rigts reserved &copy; 2017-<?php echo date("Y");?>.</span>
+                    </div>
+                    
+                @show
+                
+                
+            </div>
+
+
+        <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
+</html>
